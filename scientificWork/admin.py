fcce5b1867b0460a0ad42e6fd65403e47fd08c27
Rabbitlancer <slendercrow@mail.ru>
2017-09-28 23:40:00 +0300

    filter API barebones

--- scientificWork/admin.py
+++ scientificWork/admin.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Подключаем наши модели в админку сайта
-
-# Указываем, что из scientificWork.models импортируем конкретную модель Test_collection
 from scientificWork.models import Publication, Participation, Rand, UserProfile
+
 # Регистрируем импортированную модель в админку
 admin.site.register(Publication)
 admin.site.register(Rand)
 admin.site.register(Participation)
+admin.site.unregister(User)
 admin.site.register(UserProfile)
-
-#p = Publication.objects.all()
-# Create your models here.
